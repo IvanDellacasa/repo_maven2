18031ab95aad9a9c9cb5ced199620915d8e52fc7
IvanDellacasa <s8901786b@majorana-giorgi.edu.it>
2025-01-13 09:13:35 +0100

    Update calcolatrice.java

--- src/main/java/calcolatrice.java
+++ src/main/java/calcolatrice.java
@@ -1,14 +1,46 @@
-
-public class calcolatrice 
+public class Calcolatrice 
 {
-	public double somma(double...addendi) 
+	public double somma(double...addendi)
 	{
-		double s= 0;
+		double s = 0;
 		for(double addendo : addendi)
 		{
-			
-			s=s+Math.abs(addendo);
+			s = s + addendo;
 		}
+		
+		return s;
+	}
+	
+	public double sottrazione(double...sottraendi)
+	{
+		double s = sottraendi[0];
+		for(int i = 1; i < sottraendi.length; i++)
+		{
+			s = s - sottraendi[i];
+		}
+		
+		return s;
+	}
+	
+	public double moltiplicazione(double...moltiplicandi)
+	{
+		double s = 1;
+		for(double moltiplicando : moltiplicandi)
+		{
+			s = s * moltiplicando;
+		}
+		
+		return s;
+	}
+	
+	public double divisione(double...dividendi)
+	{
+		double s = dividendi[0];
+		for(int i = 1; i < dividendi.length; i++)
+		{
+			s = s / dividendi[i];
+		}
+		
 		return s;
 	}
 }
